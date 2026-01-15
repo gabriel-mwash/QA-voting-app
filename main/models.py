7f2084009628dcbbc1f449196af5ac82bbd5b2f8
gabu <gabujr337@gmail.com>
2026-01-15 23:55:23 +0300

    ORM for database

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class user_data(models.Model):
+    first_name = models.CharField(max_length=250)
+    second_name = models.CharField(max_length=250)
+    age = models.IntegerField()
+    nationality = models.CharField(max_length=250)
