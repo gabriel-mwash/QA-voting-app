f80621d4acbd05b23a8339c046fc79bcedcc5294
gabu <gabujr337@gmail.com>
2026-01-16 23:22:39 +0300

    using formmodels and models, super, and methods

--- main/models.py
+++ main/models.py
@@ -8,3 +8,14 @@ class user_data(models.Model):
     second_name = models.CharField(max_length=250)
     age = models.IntegerField()
     nationality = models.CharField(max_length=250)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=250)
+    email = models.CharField(max_length=250)
+
+
+class Question(models.Model):
+    question = models.TextField()
+    name = models.CharField(max_length=250, blank=True, default="Anonymous")
+    institute = models.CharField(max_length=250, blank=True, default="Anonymous")
