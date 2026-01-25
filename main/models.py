b4c825118d6298d3b70b5c7c0dbd70890f5a1204
gabu <gabujr337@gmail.com>
2026-01-25 23:53:20 +0300

    models

--- main/models.py
+++ main/models.py
@@ -3,19 +3,8 @@ from django.db import models
 # Create your models here.
 
 
-class user_data(models.Model):
-    first_name = models.CharField(max_length=250)
-    second_name = models.CharField(max_length=250)
-    age = models.IntegerField()
-    nationality = models.CharField(max_length=250)
-
-
-class User(models.Model):
-    username = models.CharField(max_length=250)
-    email = models.CharField(max_length=250)
-
-
 class Question(models.Model):
     question = models.TextField()
     name = models.CharField(max_length=250, blank=True, default="Anonymous")
-    institute = models.CharField(max_length=250, blank=True, default="Anonymous")
+    institute = models.CharField(max_length=250, blank=True,
+                                 default="Anonymous")
