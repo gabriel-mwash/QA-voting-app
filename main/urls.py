ce95ecee23169fd118fc82d9c97e613566d0f3ea
gabu <gabujr337@gmail.com>
2026-01-25 23:53:34 +0300

    urls

--- main/urls.py
+++ main/urls.py
@@ -4,9 +4,4 @@ from . import views
 
 urlpatterns = [
         path("", views.home, name="home"),
-        path("greeting/", views.greeting, name="greeting"),
-        path("form/", views.form_view,  name="form"),
-        path("success/", views.success_view, name="success"),
-        path("data/", views.data, name="data"),
-        path("form2/", views.register_view, name="form2"),
         ]
