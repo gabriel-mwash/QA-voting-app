23bedc40637e9137b348cf55a5b5e821741f2ea0
gabu <gabujr337@gmail.com>
2026-01-29 23:57:21 +0300

    success redirect

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
         path("", views.home, name="home"),
+        path("success/", views.success_view, name="success"),
+        path("questions/", views.submittedQuestions, name="questions"),
         ]
